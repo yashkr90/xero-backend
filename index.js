a6fbf641df10db65be23726401db130fb6308ecf
Yash Kumar <kryash90@gmail.com>
2023-10-03 03:32:45 +0530

    final

--- index.js
+++ index.js
@@ -46,13 +46,14 @@ app.use(
 
 const GOOGLE_CLIENT_ID =process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const SERVER_URL= process.env.SERVER_URL;
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: `${SERVER_URL}/auth/google/callback`,
     },
     function (accessToken, refreshToken, profile, done) {
       done(null, profile);
