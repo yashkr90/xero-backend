8ac026253c584b1c9ddaa8e1b170a02fb0fce6c4
Yash Kumar <kryash90@gmail.com>
2023-10-03 06:24:15 +0530

    add git9

--- src/routes/passportRoute.js
+++ src/routes/passportRoute.js
@@ -9,36 +9,38 @@ const router = express.Router();
 const CLIENT_URL = process.env.CLIENT_URL;
 
 router.get("/login/success", (req, res) => {
-
   console.log(req.user);
-    if (req.user) {
-      res.status(200).json({
-        success: true,
-        message: "successfull",
-        user: req.user,
-        //   cookies: req.cookies
-      });
-    }
-  });
-
-  router.get("/login/failed", (req, res) => {
-    res.status(401).json({
-      success: false,
-      message: "failure",
+  if (req.user) {
+    res.status(200).json({
+      success: true,
+      message: "successfull",
+      user: req.user,
+      //   cookies: req.cookies
     });
+  }
+});
+
+router.get("/login/failed", (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: "failure",
   });
+});
 
-  router.get("/google", passport.authenticate("google", { scope: ["profile","email"] }));
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-  router.get(
-    "/google/callback",
-    passport.authenticate("google", {
-      successRedirect: `${CLIENT_URL}/dashboard`,
-      failureRedirect: "/login/failed",
-    })
-  );
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: `${CLIENT_URL}/dashboard`,
+    failureRedirect: "/login/failed",
+  })
+);
 
-  router.get("/github", passport.authenticate("github", { scope: ["profile"] }));
+router.get("/github", passport.authenticate("github", { scope: ["profile"] }));
 
 router.get(
   "/github/callback",
@@ -48,4 +50,4 @@ router.get(
   })
 );
 
-  export default router;
+export default router;
